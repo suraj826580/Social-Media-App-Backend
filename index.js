91566f206464ae0ff1a3de8a713f8b6b68d216e6
suraj Yadav <111379728+suraj826580@users.noreply.github.com>
2023-05-02 15:41:41 +0530

    cors

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const { connection } = require("./configs/db");
 const { userRoute } = require("./routes/user.route");
 const { postRoute } = require("./routes/post.route");
-
+var cors = require("cors");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRoute);
